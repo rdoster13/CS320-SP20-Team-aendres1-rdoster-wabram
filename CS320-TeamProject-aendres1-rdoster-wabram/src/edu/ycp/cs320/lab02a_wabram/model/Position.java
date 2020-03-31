d78c0821823920bb638a3ab76b2fb0b556f33a80
Robert Doster <rdoster@ycp.edu>
2020-03-31 18:48:34 -0400

    Changes and interface File Added by aendres1

--- CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Position.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Position.java
@@ -1,40 +1,46 @@
 package edu.ycp.cs320.lab02a_wabram.model;
 
-public class Position { 
-    private int col; 
-    private int row; 
-    private Piece piece;
-   
-    // constructor needed to initialize board
-    public Position(int col, int row, Piece piece) {
-    	this.setCol(col);
-    	this.setRow(row);
-    	this.setPiece(piece);
-    }
+public class Position {
+	private int col;
+	private int row;
+	private Piece piece;
 
-	public int getCol() { 
-        return this.col; 
-    } 
-  
-    public void setCol(int col) { 
-        this.col = col; 
-    } 
-  
-    public int getRow() 
-    { 
-        return this.row; 
-    } 
-  
-    public void setRow(int row) 
-    { 
-        this.row = row; 
-    }
+	// constructor needed to initialize board
+	public Position(int col, int row, Piece piece) {
+		this.setCol(col);
+		this.setRow(row);
+		this.setPiece(piece);
+	}
+
+	public int getCol() {
+		return this.col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public int getRow() {
+		return this.row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
 
 	public Piece getPiece() {
 		return piece;
 	}
 
+	public boolean hasPiece() {
+		if (piece.getPieceType() == PieceType.EMPTY_SPACE) {
+			return false;
+		}
+
+		return true;
+	}
+
 	public void setPiece(Piece piece) {
 		this.piece = piece;
-	} 
-} 
+	}
+}
