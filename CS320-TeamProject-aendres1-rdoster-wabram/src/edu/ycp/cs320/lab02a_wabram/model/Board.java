1a5af7fe9ee7343274e300017b14ca957d88d473
Robert Doster <rdoster@ycp.edu>
2020-04-20 17:06:28 -0400

    Updating Board and board test calsses

--- CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Board.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Board.java
@@ -1,71 +1,59 @@
 package edu.ycp.cs320.lab02a_wabram.model;
 
+import java.awt.Point;
+
 public class Board {
-	Position[][] squares;
+	private Position[][] board;
+	private int numBoard;
 
-	public Position getPosition(int col, int row) throws Exception {
-
-		// make sure the input location is in the bounds of the board
-		if (col < 0 || col > 7 || row < 0 || row > 7) {
-			throw new Exception("Index out of bound");
-		}
-		return squares[col][row];
+	public Board() {
 	}
 
-	public void setPosition(int col, int row, Piece piece) {
-		this.squares[col][row] = squares[col][row];
-
+	public Board(int numBoard) {
+		this.numBoard = numBoard;
 	}
 
-	public void initBoard() {
-		// initialize white pieces
-		squares[0][0] = new Position(0, 0, new Rook());
-		squares[0][7] = new Position(0, 7, new Rook());
-		squares[0][1] = new Position(0, 1, new Knight());
-		squares[0][6] = new Position(0, 6, new Knight());
-		squares[0][2] = new Position(0, 2, new Bishop());
-		squares[0][5] = new Position(0, 5, new Bishop());
-		squares[0][3] = new Position(0, 3, new Queen());
-		squares[0][4] = new Position(0, 4, new King());
-
-		// initialize white pawns
-		squares[1][0] = new Position(1, 0, new Pawn());
-		squares[1][1] = new Position(1, 1, new Pawn());
-		squares[1][2] = new Position(1, 2, new Pawn());
-		squares[1][3] = new Position(1, 3, new Pawn());
-		squares[1][4] = new Position(1, 4, new Pawn());
-		squares[1][5] = new Position(1, 5, new Pawn());
-		squares[1][6] = new Position(1, 6, new Pawn());
-		squares[1][7] = new Position(1, 7, new Pawn());
-
-		// initialize black pieces
-		squares[7][0] = new Position(7, 0, new Rook());
-		squares[7][7] = new Position(7, 7, new Rook());
-		squares[7][1] = new Position(7, 1, new Knight());
-		squares[7][6] = new Position(7, 6, new Knight());
-		squares[7][2] = new Position(7, 2, new Bishop());
-		squares[7][5] = new Position(7, 5, new Bishop());
-		squares[7][3] = new Position(7, 3, new Queen());
-		squares[7][4] = new Position(7, 4, new King());
-
-		// initialize black pawns
-		squares[6][0] = new Position(6, 0, new Pawn());
-		squares[6][1] = new Position(6, 1, new Pawn());
-		squares[6][2] = new Position(6, 2, new Pawn());
-		squares[6][3] = new Position(6, 3, new Pawn());
-		squares[6][4] = new Position(6, 4, new Pawn());
-		squares[6][5] = new Position(6, 5, new Pawn());
-		squares[6][6] = new Position(6, 6, new Pawn());
-		squares[6][7] = new Position(6, 7, new Pawn());
-
-		// initialize empty squares
-		for (int i = 2; i < 6; i++) {
-			for (int j = 0; j < 8; j++) {
-				squares[i][j] = new Position(i, j, null);
+	// 2x2 array to generate an empty board.
+	// null is empty spaces
+	public void newBoard() {
+		board = new Position[8][8];
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j] = new Position(null, new Point(i, j));
 			}
 		}
 	}
+
+	// Allows us to update the board when we want / need to
+
+	public void setBoard(Position[][] board) {
+		this.board = board;
+	}
+
+	public Position getPosition(int x, int y) {
+		return this.board[x][y];
+	}
+
+	// Returns the value of the piece occupying a specified space.
+		public Piece getPiece(int x, int y) {
+			return this.board[x][y].getPiece();
+		}
+		
+	/* Every point on the board will have a matching Point coordinate (x,y) to represent the
+	 * corresponding piece. 
+	 */
+	public void setPiece(Piece piece) {
+		this.board[piece.getPosition().x][piece.getPosition().y].setPiece(piece);
+	}
+
 	
-	
+
+	public void setBoardNumber(int numBoard) {
+		this.numBoard = numBoard;
+	}
+
+	public int getBoardNumber() {
+		return numBoard;
+	}
 
 }
