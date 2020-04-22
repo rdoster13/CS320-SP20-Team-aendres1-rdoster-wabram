7974d95b59471dd7d5f4648d8baf6d92c7ed2a32
Robert Doster <rdoster@ycp.edu>
2020-04-21 23:02:23 -0400

    Finished King Logic / JUnits

--- CS320-TeamProject-aendres1-rdoster-wabram/junit/edu/ycp/cs320/lab02a_wabram/model/KingTest.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/junit/edu/ycp/cs320/lab02a_wabram/model/KingTest.java
@@ -12,28 +12,82 @@ public class KingTest {
 	private Board board = new Board();
 
 	private King testKing;
-	
+
 	@Before
 	public void setUp() {
 		testKing = new King(PieceType.KING, new Point(4, 0), 0);
 		board.newBoard();
-		board.setPiece(new King(PieceType.KING, new Point(3, 6), 0));
-		board.setPiece(new King(PieceType.KING, new Point(3, 1), 1));
+		board.setPiece(new King(PieceType.KING, new Point(2, 7), 0));
+		board.setPiece(new King(PieceType.KING, new Point(3, 4), 1));
 	}
-	
+
 	@Test
 	public void TestGetPieceType() {
 		assertEquals(testKing.getPieceType(), PieceType.KING);
 	}
-	
+
 	@Test
 	public void TestGetColor() {
 		assertEquals(testKing.getColor(), 0);
 	}
-	
+
 	@Test
 	public void TestGetLocation() {
 		assertEquals(testKing.getPosition(), new Point(4, 0));
 	}
 
+	@Test
+	public void TestCheckMove() {
+		// Check the diagonal movements 1 space in each direction
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(3, 6), board));
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(1, 6), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(4, 5), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(2, 5), board));
+
+		// Check lateral movements
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(1, 7), board));
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(3, 7), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 5), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 3), board));
+
+		// Check 1 space diagonal move with Piece in the way for Black
+		board.setPiece(new Bishop(PieceType.KING, new Point(3, 6), 0));
+		board.setPiece(new Bishop(PieceType.KING, new Point(1, 6), 0));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(3, 6), board));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(1, 6), board));
+
+		// remove the pieces
+		board.getPosition(3, 6).setPiece(null);
+		board.getPosition(1, 6).setPiece(null);
+
+		// Check 1 space diagonal move with Piece in the way for White
+		board.setPiece(new Bishop(PieceType.KING, new Point(4, 5), 1));
+		board.setPiece(new Bishop(PieceType.KING, new Point(2, 5), 1));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(4, 5), board));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(2, 5), board));
+
+		// remove the pieces
+		board.getPosition(4, 5).setPiece(null);
+		board.getPosition(2, 5).setPiece(null);
+
+		// Check 1 lateral space move with Piece in the way for Black
+		board.setPiece(new Bishop(PieceType.KING, new Point(3, 7), 0));
+		board.setPiece(new Bishop(PieceType.KING, new Point(1, 7), 0));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(3, 7), board));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(1, 7), board));
+
+		// remove the pieces
+		board.getPosition(3, 7).setPiece(null);
+		board.getPosition(1, 7).setPiece(null);
+
+		// Check 1 lateral space move with Piece in the way for White
+		board.setPiece(new Bishop(PieceType.KING, new Point(4, 4), 1));
+		board.setPiece(new Bishop(PieceType.KING, new Point(2, 4), 1));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(4, 4), board));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(2, 4), board));
+
+		// remove the pieces
+		board.getPosition(4, 4).setPiece(null);
+		board.getPosition(2, 4).setPiece(null);
+	}
 }
--- CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/King.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/King.java
@@ -3,11 +3,10 @@ package edu.ycp.cs320.lab02a_wabram.model;
 import java.awt.Point;
 
 public class King extends Piece {
-	/* validateCheck(): boolean
-	 * validateCheckMate(): boolean 
-	 * verifyCastle(): boolean
+	/*
+	 * validateCheck(): boolean validateCheckMate(): boolean verifyCastle(): boolean
 	 */
-	
+
 	public King(PieceType type, Point position, int color) {
 		super(type, position, color);
 	}
@@ -15,21 +14,34 @@ public class King extends Piece {
 	public boolean validateCheck() {
 		return false;
 	}
-	
+
 	public boolean validateCheckMate() {
 		return false;
 	}
-	
+
 	public boolean validateCastle() {
-		/* make sure there are no pieces between king and rook, 
-		 * and that neither piece has moved yet. 
+		/*
+		 * make sure there are no pieces between king and rook, and that neither piece
+		 * has moved yet.
 		 */
 		return false;
 	}
 
 	@Override
 	public boolean checkMove(Point point, Board board) {
-		// TODO Auto-generated method stub
+		// make sure the space is either empty OR
+		// is not occupied by the same color piece.
+		if (board.getPosition(point.x, point.y).getPiece() != null) {
+			if (board.getPosition(point.x, point.y).getPiece().getColor() == board.getPosition(position.x, position.y)
+					.getPiece().getColor()) {
+				return false;
+			}
+		}
+		// Check that the king can only move one space in either direction. 
+		// if either the X or Y coordinate is more that one from the current position, invalid. 
+		if (point.x - position.x > 1 || point.y - position.y > 1) {
+			return false;
+		}
 		return true;
 	}
 }
