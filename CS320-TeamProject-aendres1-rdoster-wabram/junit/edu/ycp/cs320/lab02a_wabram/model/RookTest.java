4c19e6cd2664ff6c4ecfb01fed5fbec393822166
Robert Doster <rdoster@ycp.edu>
2020-04-21 22:37:57 -0400

    Rook Logic / Test Cases Added

--- CS320-TeamProject-aendres1-rdoster-wabram/junit/edu/ycp/cs320/lab02a_wabram/model/RookTest.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/junit/edu/ycp/cs320/lab02a_wabram/model/RookTest.java
@@ -12,28 +12,108 @@ public class RookTest {
 	private Board board = new Board();
 
 	private Rook testRook;
-	
+
 	@Before
 	public void setUp() {
-		testRook = new Rook(PieceType.ROOK, new Point(4, 0), 0);
+		testRook = new Rook(PieceType.ROOK, new Point(0, 0), 0);
 		board.newBoard();
-		board.setPiece(new Rook(PieceType.ROOK, new Point(3, 6), 0));
-		board.setPiece(new Rook(PieceType.ROOK, new Point(3, 1), 1));
+		board.setPiece(new Rook(PieceType.ROOK, new Point(2, 7), 0));
+		board.setPiece(new Rook(PieceType.ROOK, new Point(3, 4), 1));
 	}
-	
+
 	@Test
 	public void TestGetPieceType() {
 		assertEquals(testRook.getPieceType(), PieceType.ROOK);
 	}
-	
+
 	@Test
 	public void TestGetColor() {
 		assertEquals(testRook.getColor(), 0);
 	}
-	
+
 	@Test
 	public void TestGetLocation() {
-		assertEquals(testRook.getPosition(), new Point(4, 0));
+		assertEquals(testRook.getPosition(), new Point(0, 0));
 	}
 
+	@Test
+	public void TestCheckMove() {
+		// Check the diagonal movements 1 space in each direction
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(1, 7), board));
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(3, 7), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 5), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 3), board));
+
+		// Check the diagonal movements 2 space in each direction
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(0, 7), board));
+		assertTrue(board.getPiece(2, 7).checkMove(new Point(4, 7), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 6), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 2), board));
+
+		// Check the diagonal movements 3 space in each direction
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 1), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(3, 7), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(6, 4), board));
+		assertTrue(board.getPiece(3, 4).checkMove(new Point(0, 4), board));
+
+		// Check 1 space move with Piece in the way for Black
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(3, 7), 0));
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(1, 7), 0));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(3, 7), board));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(1, 7), board));
+
+		// remove the pieces
+		board.getPosition(3, 7).setPiece(null);
+		board.getPosition(1, 7).setPiece(null);
+
+		// Check 1 space move with Piece in the way for White
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(4, 4), 1));
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(2, 4), 1));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(4, 4), board));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(2, 4), board));
+
+		// remove the pieces
+		board.getPosition(4, 4).setPiece(null);
+		board.getPosition(2, 4).setPiece(null);
+
+		// Check 2 space move with Piece in the way for Black
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(4, 7), 0));
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(0, 7), 0));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(4, 7), board));
+		assertFalse(board.getPiece(2, 7).checkMove(new Point(0, 7), board));
+
+		// remove the pieces
+		board.getPosition(4, 7).setPiece(null);
+		board.getPosition(0, 7).setPiece(null);
+
+		// Check 2 space move with Piece in the way for White
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(3, 2), 1));
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(3, 6), 1));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(3, 2), board));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(3, 6), board));
+
+		// remove the pieces
+		board.getPosition(3, 2).setPiece(null);
+		board.getPosition(3, 6).setPiece(null);
+
+		// Check 3 space move with Piece in the way for White
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(6, 4), 1));
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(0, 4), 1));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(6, 4), board));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(0, 4), board));
+
+		// remove the pieces
+		board.getPosition(6, 4).setPiece(null);
+		board.getPosition(0, 4).setPiece(null);
+
+		// Check 3 space move with Piece in the way for White
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(3, 7), 1));
+		board.setPiece(new Bishop(PieceType.ROOK, new Point(3, 1), 1));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(3, 7), board));
+		assertFalse(board.getPiece(3, 4).checkMove(new Point(3, 1), board));
+
+		// remove the pieces
+		board.getPosition(3, 7).setPiece(null);
+		board.getPosition(3, 1).setPiece(null);
+	}
 }
--- CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Rook.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Rook.java
@@ -9,8 +9,64 @@ public class Rook extends Piece {
 	}
 
 	@Override
-	public	boolean checkMove(Point point, Board board) {
-		// TODO Auto-generated method stub
+	public boolean checkMove(Point point, Board board) {
+		// Check that the destination is not the same as the starting space
+		if (position.x == point.x && position.y == point.y) {
+			return false;
+		}
+
+		// check that the move is not diagonal. if it is, return false
+		if (position.y != point.y && position.x != point.x) {
+			return false;
+		}
+
+		// make sure the space is either empty OR
+		// is not occupied by the same color piece.
+		if (board.getPosition(point.x, point.y).getPiece() != null) {
+			if (board.getPosition(point.x, point.y).getPiece().getColor() == board.getPosition(position.x, position.y)
+					.getPiece().getColor()) {
+				return false;
+			}
+		}
+
+		// Check each of the directions using the loops
+		// the rook can move any number of spaces (as long as it is not blocked)
+		// Check Left
+		if (position.x > point.x) {
+			for (int i = point.x + 1; i < position.x; i++) {
+				if (board.getPosition(i, point.y).getPiece() != null) {
+					return false;
+				}
+			}
+		}
+		
+		// Check Right
+		if (position.x < point.x) {
+			for (int i = position.x + 1; i < point.x; i++) {
+				if (board.getPosition(i, point.y).getPiece() != null) {
+					return false;
+				}
+			}
+		}
+		
+		// Check Up
+		if (position.y > point.y) {
+			for (int i = point.y + 1; i < position.y; i++) {
+				if (board.getPosition(point.x, i).getPiece() != null) {
+					return false;
+				}
+			}
+		}
+
+		// Check Down
+		if (position.y < point.y) {
+			for (int i = position.y + 1; i < point.y; i++) {
+				if (board.getPosition(point.x, i).getPiece() != null) {
+					return false;
+				}
+			}
+		}
+
 		return true;
 	}
 
