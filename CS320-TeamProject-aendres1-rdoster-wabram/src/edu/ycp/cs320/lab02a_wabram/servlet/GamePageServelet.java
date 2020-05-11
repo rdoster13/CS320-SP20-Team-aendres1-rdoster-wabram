82ea3d8e2460d14ec179f185f9eeaf7c9299064e
Robert Doster <rdoster@ycp.edu>
2020-05-11 12:05:03 -0400

    Self Check Issues TS, Not completed.
    not sure what is going on here, but when you take an oppenent's piece
    with your king
    
    it replaces the king with the original piece of the same color that was
    on the board.
    
    I positioned the pieces so you can force a check wiht the white queen,
    and test it.

--- CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/servlet/GamePageServelet.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/servlet/GamePageServelet.java
@@ -124,6 +124,10 @@ public class GamePageServelet extends HttpServlet {
 					if (game.getBoard().getPosition(startX, startY).getPiece().checkMove(new Point(endX, endY),
 							game.getBoard()) == true) {
 
+						// not sure what is going on here, but when you take an oppenent's piece with your king
+						// it replaces the king with the original piece of the same color that was on the board. 
+						// Issue is either with the placement of this method or something in the Self-Check method itself. not sure.
+						
 						// check that move does not put you in check
 						if (controller.evaluateSelfCheck(
 								game.getBoard().getPosition(startX, startY).getPiece().getColor(),
@@ -131,22 +135,10 @@ public class GamePageServelet extends HttpServlet {
 								game.getBoard().getPosition(endX, endY)) == false) {
 							check = false;
 						}
-						if (controller.evaluateOppCheck(
-								game.getBoard().getPosition(startX, startY).getPiece().getColor(),
-								game.getBoard().getPosition(startX, startY),
-								game.getBoard().getPosition(endX, endY)) == true) {
-							/*
-							 * System.out.print("\n**********************************************");
-							 * System.out.print("\nYou are in check!");
-							 * System.out.print("\n**********************************************");
-							 * System.out.print("\n");
-							 */
-
-							// add message / status to say check
-							check = true;
-						}
+						
 
 						if (check == false) {
+														
 							// if statement to check on color of piece / move off of board
 							if (game.getBoard().getPosition(endX, endY).getPiece() != null
 									&& game.getBoard().getPosition(startX, startY).getPiece().getColor() != game
@@ -156,7 +148,22 @@ public class GamePageServelet extends HttpServlet {
 								controller.takePiece(game.getBoard().getPosition(endX, endY),
 										game.getBoard().getPosition(0, 8));
 							}
+							
+							// check if move 
+							if (controller.evaluateOppCheck(
+									game.getBoard().getPosition(startX, startY).getPiece().getColor(),
+									game.getBoard().getPosition(startX, startY),
+									game.getBoard().getPosition(endX, endY)) == true) {
+								/*
+								 * System.out.print("\n**********************************************");
+								 * System.out.print("\nYou are in check!");
+								 * System.out.print("\n**********************************************");
+								 * System.out.print("\n");
+								 */
 
+								// add message / status to say check
+								check = true;
+							}
 							// controller.updatePieceLocation(startX, startY, endX, endY);
 							controller.movePiece(game.getBoard().getPosition(startX, startY),
 									game.getBoard().getPosition(endX, endY));
