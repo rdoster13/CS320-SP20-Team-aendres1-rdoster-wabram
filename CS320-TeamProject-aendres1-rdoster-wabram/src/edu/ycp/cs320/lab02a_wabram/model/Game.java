979f81b6841c54181cf2a6af709a267f38433a63
Robert Doster <rdoster@ycp.edu>
2020-05-12 23:27:22 -0400

    Set up the JUnits for GameTset,PlayerTest, Deleted Log/ LogTest

--- CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Game.java
+++ CS320-TeamProject-aendres1-rdoster-wabram/src/edu/ycp/cs320/lab02a_wabram/model/Game.java
@@ -42,26 +42,21 @@ public class Game {
 
 	}
 
-	public String getMoves() {
-		return moves;
-	}
-
-	public void setMoves(Piece piece, int col, int row) {
-		this.moves = piece + " to " + col + row + ". ";
-	}
-
+	/*
+	 * public String getMoves() { return moves; }
+	 * 
+	 * public void setMoves(Piece piece, int col, int row) { this.moves = piece +
+	 * " to " + col + row + ". "; }
+	 */
 	public Board getBoard() {
 		return board;
 	}
 
-	/*public void setGameNumber(int gameNum) {
-		this.gameNum = gameNum;
-	}
-	
-
-	public int getGameNumber() {
-		return gameNum;
-	}
+	/*
+	 * public void setGameNumber(int gameNum) { this.gameNum = gameNum; }
+	 * 
+	 * 
+	 * public int getGameNumber() { return gameNum; }
 	 */
 	public int getTurn() {
 		return turn;
@@ -80,34 +75,24 @@ public class Game {
 	}
 
 	/*
-	public void newGame() {
-		// 0 = white and 1 = black
-		// create pawns
-		for (int i = 0; i < 8; i++) {
-			board.setPiece(new Pawn(PieceType.PAWN, new Point(i, 6), 0));
-			board.setPiece(new Pawn(PieceType.PAWN, new Point(i, 1), 1));
-		}
-
-		// 0 = white and 1 = black
-		// create Bishop, Knights, Rooks
-		for (int i = 0; i < 2; i++) {
-			board.setPiece(new Bishop(PieceType.BISHOP, new Point((i * 3) + 2, 0), 1));
-			board.setPiece(new Bishop(PieceType.BISHOP, new Point((i * 3) + 2, 7), 0));
-			board.setPiece(new Knight(PieceType.KNIGHT, new Point((i * 5) + 1, 0), 1));
-			board.setPiece(new Knight(PieceType.KNIGHT, new Point((i * 5) + 1, 7), 0));
-			board.setPiece(new Rook(PieceType.ROOK, new Point((i * 7), 0), 1));
-			board.setPiece(new Rook(PieceType.ROOK, new Point((i * 7), 7), 0));
-		}
-
-		// 0 = white and 1 = black
-		// Create Queens
-		board.setPiece(new Queen(PieceType.QUEEN, new Point(4, 0), 1));
-		board.setPiece(new Queen(PieceType.QUEEN, new Point(4, 7), 0));
-
-		// 0 = white and 1 = black
-		// Create Kings
-		board.setPiece(new King(PieceType.KING, new Point(3, 0), 1));
-		board.setPiece(new King(PieceType.KING, new Point(3, 7), 0));
-	}
-	*/
+	 * public void newGame() { // 0 = white and 1 = black // create pawns for (int i
+	 * = 0; i < 8; i++) { board.setPiece(new Pawn(PieceType.PAWN, new Point(i, 6),
+	 * 0)); board.setPiece(new Pawn(PieceType.PAWN, new Point(i, 1), 1)); }
+	 * 
+	 * // 0 = white and 1 = black // create Bishop, Knights, Rooks for (int i = 0; i
+	 * < 2; i++) { board.setPiece(new Bishop(PieceType.BISHOP, new Point((i * 3) +
+	 * 2, 0), 1)); board.setPiece(new Bishop(PieceType.BISHOP, new Point((i * 3) +
+	 * 2, 7), 0)); board.setPiece(new Knight(PieceType.KNIGHT, new Point((i * 5) +
+	 * 1, 0), 1)); board.setPiece(new Knight(PieceType.KNIGHT, new Point((i * 5) +
+	 * 1, 7), 0)); board.setPiece(new Rook(PieceType.ROOK, new Point((i * 7), 0),
+	 * 1)); board.setPiece(new Rook(PieceType.ROOK, new Point((i * 7), 7), 0)); }
+	 * 
+	 * // 0 = white and 1 = black // Create Queens board.setPiece(new
+	 * Queen(PieceType.QUEEN, new Point(4, 0), 1)); board.setPiece(new
+	 * Queen(PieceType.QUEEN, new Point(4, 7), 0));
+	 * 
+	 * // 0 = white and 1 = black // Create Kings board.setPiece(new
+	 * King(PieceType.KING, new Point(3, 0), 1)); board.setPiece(new
+	 * King(PieceType.KING, new Point(3, 7), 0)); }
+	 */
 }
